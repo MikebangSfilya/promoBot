54b70025385fb3e6e35240098d58ae2c0da88733
Sfilya <Typegoot@gmail.com>
2025-12-09 14:32:04 +0300

    smallfix

--- strings.go
+++ strings.go
@@ -4,7 +4,7 @@ func init() {
 	locpool.Resources["en"] = map[string]string{
 		"commands.default.message.on.command": "Unknown command",
 		"callbacks.error":                     "The state was lost 😢",
-		"commands.default.message":            "It appears you haven't invoked any of the bot's support commands. Call /help for assistance.",
+		"commands.default.message":            "It appears you haven't invoked any of the bot's support commands. Call /promo for create promocode.",
 
 		"commands.promo.fields.promo": "Vvdedite promic, pendosLanguage",
 		"commands.promo.description":  "Generate promoCode for dickGrowerBot",
@@ -23,7 +23,7 @@ func init() {
 		"commands.default.message.on.command": "Неизвестная команда",
 		"callbacks.error":                     "Состояние формы потерялось 😢",
 
-		"commands.default.message": "Кажется вы не вызвали ни одной команды которую поддерживает бот. Вызовите /help для помощи.",
+		"commands.default.message": "Кажется вы не вызвали ни одной команды которую поддерживает бот. Вызовите /promo для создания промокода.",
 
 		"commands.help.description":     "показать помощь",
 		"commands.help.fields.language": "Выберите свой язык:",
